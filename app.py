58873bcc073033b62f26c1294b3d487e7c553a21
Safaa-Mostafa2 <134886794+Safaa-Mostafa2@users.noreply.github.com>
2023-05-29 02:14:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,7 @@ import numpy as np
 import pickle
 from sklearn.preprocessing import MinMaxScaler
 import os
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from PIL import Image
-import tensorflow as tf
-# Flask utils
-from werkzeug.utils import secure_filename
-from heatmap import save_and_display_gradcam,make_gradcam_heatmap
+
 app = Flask(__name__)
 api = Api(app)
 model3=pickle.load(open('model.pkl','rb'))
@@ -29,80 +23,7 @@ def convertTuple(tup):
     for item in tup:
         str = str + item
     return str
- 
- 
- 
-# skin cancer start  
 
-
-
-
-
-app.config['HEATMAP_FOLDER'] = 'heatmap'
-app.config['UPLOAD_FOLDER'] = 'uploads'
-# Model saved with Keras model.save()
-MODEL_PATH = './model_v1.h5'
-
-
-#Load your trained model
-model = load_model(MODEL_PATH)
-
-
-
-class_dict = {0:"Basal_Cell_Carcinoma (Cancer)",
-             1:"Melanoma (Cancer)",
-             2:"Nevus (Non-Cancerous)"}
-
-@app.route('/uploads/<filename>')
-def upload_img(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-        
-
-
-def model_predict(img_path, model):
-    img = Image.open(img_path).resize((224,224)) #target_size must agree with what the trained model expects!!
-
-    # Preprocessing the image
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = img.astype('float32')/255
-    preds = model.predict(img)[0]
-    prediction = sorted(
-      [(class_dict[i], round(j*100, 2)) for i, j in enumerate(preds)],
-      reverse=True,
-      key=lambda x: x[1]
-  )
-    return prediction,img
-
-
-@app.route('/predict', methods=[ 'POST'])
-def predict(): 
-
-        f = request.files['file']
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-        basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-        file_name=os.path.basename(file_path)
-        # Make prediction
-        pred,img = model_predict(file_path, model)
-        last_conv_layer_name = "block_16_depthwise"
-        heatmap = make_gradcam_heatmap(img, model, last_conv_layer_name)
-        fname=save_and_display_gradcam(file_path, heatmap)
-
-        classA = pred[0][0]
-        classB = pred[1][0]
-        classC = pred[2][0]
-        return jsonify({
-           "apiStatus":"true",
-           classA:pred[0][1],
-           classB:pred[1][1],
-           classC:pred[2][1]
-         # preds:pred
-          })
-
- 
 @app.route('/predictHeart',methods=['post'])
 def post():
         posted_data = request.get_json()
